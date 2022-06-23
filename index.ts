5bac2d4e0d6e53e32fc5e37bb4066ffc4dd3a23d
Viacheslav Tiulkov <tyli008@mail.ru>
2022-06-23 13:41:04 +0300

    feat: add correct finish for websocket

--- index.ts
+++ index.ts
@@ -2,8 +2,20 @@ import { httpServer } from './src/http_server/index';
 import { startServer } from './src/ws_server/server';
 import 'dotenv/config';
 
-const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8080;
+const PORT = process.env.PORT || 8080;
 
 console.log(`Start static http server on the ${PORT} port!`);
+
 httpServer.listen(PORT);
-startServer(httpServer);
+const wss = startServer(httpServer);
+
+process.on('exit', () => {
+  console.log('terminate all client connection');
+  for (const client of wss.clients) {
+    client.close();
+  }
+});
+
+process.on('SIGINT', () => {
+  process.exit();
+});
