6c0fd480b7437c36e4d88a2367b032ee9d1db7f8
Viacheslav Tiulkov <tyli008@mail.ru>
2022-06-23 21:14:41 +0300

    feat: rewrite to stream

--- src/ws_server/server.ts
+++ src/ws_server/server.ts
@@ -1,4 +1,4 @@
-import { AddressInfo, WebSocketServer } from 'ws';
+import { AddressInfo, createWebSocketStream, WebSocketServer } from 'ws';
 import { handler } from './router';
 import { Server } from 'http';
 
@@ -8,15 +8,22 @@ export function startServer(server: Server) {
   });
   console.log();
   wss.on('connection', ws => {
-    ws.on('message', async message => {
-      try {
-        console.log(message.toString());
-        const [command] = message.toString().split(' ');
-        const result = await handler(message);
-        ws.send(`${command} ${result ?? ''}`);
-      } catch (e) {
-        console.log('Error has occurred', e);
+    // ws.on('message', async message => {
+    const stream = createWebSocketStream(ws);
+    stream.on('readable', () => {
+      const buffer: any = [];
+      let data;
+      while ((data = stream.read())) {
+        buffer.push(data);
       }
+      const message = buffer.join('');
+      const result = handler(message);
+      console.log(message.toString());
+      const [command] = message.toString().split(' ');
+      stream.write(`${command} ${result ?? ''}`);
+    });
+    stream.on('error', e => {
+      console.log(`Error has occured ${e}`);
     });
   });
   console.log(`WS server started on the ${(wss.address() as AddressInfo).port} port`);
