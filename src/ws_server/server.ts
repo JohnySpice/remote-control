b4066d250702dd653545ebdecd2776f635102dd6
Viacheslav Tiulkov <tyli008@mail.ru>
2022-06-25 11:16:56 +0300

    fix: fix socket output data

--- src/ws_server/server.ts
+++ src/ws_server/server.ts
@@ -6,24 +6,22 @@ export function startServer(server: Server) {
   const wss = new WebSocketServer({
     server,
   });
-  console.log();
   wss.on('connection', ws => {
-    // ws.on('message', async message => {
-    const stream = createWebSocketStream(ws);
-    stream.on('readable', () => {
+    const stream = createWebSocketStream(ws, { decodeStrings: false });
+    stream.on('readable', async () => {
       const buffer: any = [];
       let data;
       while ((data = stream.read())) {
         buffer.push(data);
       }
       const message = buffer.join('');
-      const result = handler(message);
+      const result = await handler(message);
       console.log(message.toString());
       const [command] = message.toString().split(' ');
       stream.write(`${command} ${result ?? ''}`);
     });
     stream.on('error', e => {
-      console.log(`Error has occured ${e}`);
+      console.log(`Error has occured: ${e}`);
     });
   });
   console.log(`WS server started on the ${(wss.address() as AddressInfo).port} port`);
